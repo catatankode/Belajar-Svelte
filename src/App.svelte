3b0bafc614cbb1fb9c1f0b5b8a05ea1b3e5e287f
rendiwijiatmoko <rendiwijiatmoko97@gmail.com>
2020-01-20 17:51:34 +0700

    hide

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,24 @@
 <script>
 	const name = "Rendi Wijiatmoko";
 	let point = 0;
+	let show = false;
 	const increase = () => point += 1;
 	const decrese = () => point -= 1;
+	const showButton = () => show = !show;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Nilai awal {point}</p>
-	<button on:click={increase}>add</button>
-	<button on:click={decrese}>decrese</button>
-	<input type="number" bind:value={point}/>
+	<button on:click={showButton}>
+		{#if show}hide{:else}show{/if}
+	</button> <br />
+
+	{#if show }
+		<button on:click={increase}>add</button>
+		<button on:click={decrese}>decrese</button>
+		<input type="number" bind:value={point}/>
+	{/if}
 </main>
 
 <style>
