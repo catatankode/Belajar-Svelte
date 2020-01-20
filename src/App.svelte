f1ed43b9bdd37d96bfe44871c4ca8a2a86b82c4e
rendiwijiatmoko <rendiwijiatmoko97@gmail.com>
2020-01-20 16:53:28 +0700

    test props

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
 	export let name;
+	export let addr;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Visit my room at {addr}</p>
 </main>
 
 <style>
