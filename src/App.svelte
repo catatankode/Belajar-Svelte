cea0dd1b80c63bf4d59cbeb6b7e6f34aaa851ea3
rendiwijiatmoko <rendiwijiatmoko97@gmail.com>
2020-01-21 11:34:26 +0700

    cheat

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,63 @@
 <script>
-	const name = "Rendi Wijiatmoko";
-	let point = 0;
-	let show = false;
-	const increase = () => point += 1;
-	const decrese = () => point -= 1;
-	const showButton = () => show = !show;
+  import Navbar from "./Navbar.svelte";
+  import Player from "./Player.svelte";
+  import AddPlayer from "./AddPlayer.svelte";
+
+  let players = [
+    {
+      name: "Rendi",
+      points: 53
+    },
+    {
+      name: "Wijiatmoko",
+      points: 45
+    },
+    {
+      name: "Momo",
+      points: 34
+    }
+  ];
+
+  const addPlayer = e => {
+    const newPlayer = e.detail;
+    players = [...players, newPlayer];
+  };
+
+  const removePlayer = e => {
+    players = players.filter(player => player.name !== e.detail);
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Nilai awal {point}</p>
-	<button on:click={showButton}>
-		{#if show}hide{:else}show{/if}
-	</button> <br />
-
-	{#if show }
-		<button on:click={increase}>add</button>
-		<button on:click={decrese}>decrese</button>
-		<input type="number" bind:value={point}/>
-	{/if}
+  <Navbar />
+  <div class="container">
+    <AddPlayer on:addplayer={addPlayer} />
+    {#if players.length === 0}
+      <p>No Players</p>
+    {:else}
+      {#each players as player}
+        <Player
+          name={player.name}
+          points={player.points}
+          on:removeplayer={removePlayer} />
+      {/each}
+    {/if}
+  </div>
 </main>
 
 <style>
-	main {
+main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
-
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
-
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
---
+++ src/Navbar.svelte
@@ -0,0 +1,22 @@
+<main>
+  <div class="mo">
+    <h1>Belajar</h1>
+  </div>
+</main>
+
+<style>
+.mo {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 1;
+  width: 100%;
+  opacity: 0.9;
+  background-color: #204f6e;
+  margin-bottom: 10px;
+}
+h1 {
+  color: white;
+  margin: 10px;
+}
+</style>
